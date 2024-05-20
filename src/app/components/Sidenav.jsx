04a429d14a298e470df70acb50335f600ecdbba3
Azroph <falgitoine@gmail.com>
2024-05-20 16:54:22 +0200

    init sidnav

---
+++ src/app/components/Sidenav.jsx
@@ -0,0 +1,20 @@
+import react from "react";
+
+export default function Sidenav(){
+    return(
+        <div>
+            <p>pouette</p>
+            <ul>
+                <li>
+                    Mon compte
+                </li>
+                <li>
+                    Classement
+                </li>
+                <li>
+                    Mes notes
+                </li>
+            </ul>
+        </div>
+    )
+}
